69e0555c3f224a13f35e8b534751375816bab60e
Kangas Jyri V <jyri_kangas@outlook.com>
2023-01-19 16:14:23 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -21,7 +21,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     const newDate = parseTemporalDate(req.query.date)
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, date, baseCost, newDate);
     res.json({ cost });
   });
 
@@ -36,7 +36,7 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date, baseCost) {
+  function calculateCost(age, type, date, baseCost, newDate) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
