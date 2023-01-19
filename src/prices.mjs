659f0c344ab12aefb3519947d58869ee4bd2ce5a
Kangas Jyri V <jyri_kangas@outlook.com>
2023-01-19 16:21:55 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -98,7 +98,8 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
-      if (date && date.year === holiday.year 
+      if (date 
+        && date.year === holiday.year 
         && date.month === holiday.month 
         && date.day === holiday.day) {}
     }
